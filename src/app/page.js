6cff9b24cf0e7c8a6d9b5b54f758f143591b401f
Danil Bugaenko <danilbugaenko.work@gmail.com>
2023-10-18 10:20:51 +0200

    feat: ts + rtk query implement

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-import { Location } from "@/Components/Location/Location";
-import { Cart } from "@/features/Cart/Cart";
-import Carousel from "@/features/Carousel/Carousel";
-import { HeaderCart } from "@/features/HeaderCart/HeaderCart";
-
-export default function Page() {
-  return <></>;
-}
