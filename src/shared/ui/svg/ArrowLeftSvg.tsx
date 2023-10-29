4f49343b3aeadcdb93fb5df7dad4fb0da21a6eda
frontsinfin <siniyfilin@yandex.ru>
2023-10-29 15:54:42 +0300

    fix: SVG converted to SVGR

---
+++ src/shared/ui/svg/ArrowLeftSvg.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+const ArrowLeftSvg = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={12}
+    height={14}
+    fill="none"
+    viewBox="0 0 8 10"
+  >
+    <path
+      fill="#1B1F29"
+      d="M.983 3.431a2 2 0 0 0 0 3.138l3.535 2.796c1.311 1.037 3.24.103 3.24-1.569V2.204C7.759.532 5.83-.402 4.519.635L.983 3.431Z"
+    />
+  </svg>
+);
+export default ArrowLeftSvg;
